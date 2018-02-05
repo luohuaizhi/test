a79ca7bc4749c2b79a48dc5e3c1c151872197dc8
luohuaizhi <luohuaizhi@9ffenqigo.com>
2018-02-05 16:45:47 +0800

    完善string和int 的基本操作

--- testRedis.py
+++ testRedis.py
@@ -1,5 +1,6 @@
 # encoding:utf-8
 import redis
+import time
 
 
 def main():
@@ -12,15 +13,41 @@ def main():
     try:
         key = "TEST_KEY_1484"
         # string
-        # print redis_cli.set(key, "5555555555")
-        # print redis_cli.get(key)
-        # print redis_cli.strlen(key)
-        # print redis_cli.delete(key)
-        # print redis_cli.delete(key)
+        print redis_cli.delete(key)
+        print redis_cli.set(key, "hello")
+        print redis_cli.get(key)
+        print redis_cli.append(key, "world")
+        print redis_cli.get(key)
+        print redis_cli.strlen(key)
+        # timeout
+        print redis_cli.delete(key)
+        print redis_cli.setex(key, 10, "hello")
+        print redis_cli.get(key)
+        print redis_cli.ttl(key)
+        # time.sleep(10)
+        print redis_cli.get(key)
+        # not set
+        print redis_cli.delete(key)
+        print redis_cli.setnx(key, "hello") # 设置key为hello
+        print redis_cli.setnx(key, "world") # 设置key为world，key已经有值，设置失败
+        print redis_cli.get(key) # 只能得到第一次设置的值
+        print redis_cli.setrange(key, 20, "111111") # 从20位置开始替换，若索引值超过原值长度，则补0
+        print redis_cli.getrange(key, 1, -1) # 获取1到-1（最后）的子串
+        print redis_cli.mset({"key1":1, "key2":2, "key3":3})
+        print redis_cli.mget("key1", "key2", "key3")
+        print redis_cli.msetnx({"key3":7, "key4":8, "key5":9}) # 已经有值的key会设置失败，从而导致整次设置失败
+        print redis_cli.mget("key1", "key2", "key3", "key4", "key5")
+
+
         # integer
-        # print redis_cli.set(key, 1)
-        # print redis_cli.get(key)
-        # print redis_cli.strlen(key)
+        print redis_cli.delete(key)
+        print redis_cli.set(key, 1)
+        print redis_cli.incr(key)  # 累加1
+        print redis_cli.incr(key, 2)  # 累加2
+        print redis_cli.incrby(key, 5) # 累加5
+        print redis_cli.decr(key) # 累减1
+        print redis_cli.decr(key, 3) # 累减3
+        print redis_cli.getset(key, 0) #获取旧值，并获取新值，一次原子操作
         
         # list
         print redis_cli.delete(key) # 删除key
@@ -67,8 +94,8 @@ def main():
         print redis_cli.smembers(key) # 查看成员
         key1 = key+"B"
         print redis_cli.sadd(key1, 9,8,7)
-        print redis_cli.move(key, key1, 2) # 将2从key移到key1
-        print redis_cli.move(key, key1, 2) # 将2从key移到key1, 由于此时key中没有2，移动失败
+        print redis_cli.smove(key, key1, 2) # 将2从key移到key1
+        print redis_cli.smove(key, key1, 2) # 将2从key移到key1, 由于此时key中没有2，移动失败
         print redis_cli.smembers(key) # 查看成员
         print redis_cli.smembers(key1) # 查看成员
         # 与、或、非
@@ -89,11 +116,11 @@ def main():
 
         # Sorted-Sets和Sets类型极为相似,各种操作命令也相似
         print redis_cli.delete(key)
-        print redis_cli.zadd(key, 1,2,3) # 添加、设置成员
+        print redis_cli.zadd(key, "1",1,"2",2) # 添加、设置成员
         print redis_cli.zcard(key) # 获取成员数量
-        print redis_cli.zismember(key, 4) # 判断是否包含该成员
-        print redis_cli.zrandmember(key) # 随机获取一个成员，成员依然在Set中存在
-        print redis_cli.zmembers(key) # 查看成员
+        print redis_cli.zscan(key, 4) # 查看成员
+        # print redis_cli.zrange(key) # 
+        # print redis_cli.zcount(key) # 
     except Exception as e:
         print e.message
     finally:
