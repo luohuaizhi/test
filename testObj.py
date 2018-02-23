b6ef5dae3c90f09b343d5c3aae4605eda12add1b
luohuaizhi <luohuaizhi@9ffenqigo.com>
2018-02-23 17:58:03 +0800

    测试未定义属性

--- testObj.py
+++ testObj.py
@@ -5,6 +5,10 @@ class Obj(object):
     def __call__(self, name):
         print "__call__ of %s" % name
 
+    def __new__(cls, name):
+        print "__new__"
+        return super(Obj, cls).__new__(cls)
+
     # def __new__(cls, name, bases, attrs):
     #     print "__new__"
     #     return super(Obj, cls).__new__(cls, name, bases, attrs)
@@ -16,12 +20,20 @@ class Obj(object):
     def __del__(self):
         print "I'm del"
 
+    def mydefault(self, *args):
+        print "default"        
+
+    def __getattr__(self, name):
+        print "call fn: ", name
+        return self.mydefault
+
 
 
 def main():
     obj = Obj("test")
     print obj.name
     obj("james")
+    obj.tt()
 
 
 if __name__ == '__main__':
