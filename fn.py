71cd7cc767b77b58e58dd0f593564b0cefe48dd3
luohuaizhi <luohuaizhi@9ffenqigo.com>
2018-02-27 18:25:45 +0800

    python中类方法、类实例方法、静态方法

--- fn.py
+++ fn.py
@@ -1,9 +1,26 @@
 
+class A():
+    cnt = 1
+
+    def fn1(self, x):
+        print "fn1", self.cnt, x
+
+    @classmethod
+    def fn2(cls, x):
+        print "fn2", cls.cnt, x
+
+    @staticmethod
+    def fn3(x):
+        print "fn3", x
+
 def main():
-	if 1 == 1:
-		a="100"
-	b=a
-	print b
+    a=A()
+    a.fn1(5)
+    a.fn2(5)
+    a.fn3(5)
+    A.fn3(5)
+    A.fn2(5)
+    A.fn1(5)
 
 if __name__ == '__main__':
-	main()
+    main()
